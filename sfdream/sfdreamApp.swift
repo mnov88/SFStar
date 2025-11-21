9f898fee31f0519154c3b303f2471f1b21659661
mnov88 <47901419+mnov88@users.noreply.github.com>
2025-11-21 20:13:52 +0100

    Initial Commit

---
+++ sfdream/sfdreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfdreamApp.swift
+//  sfdream
+//
+//  Created by Milos Novovic on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct sfdreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
